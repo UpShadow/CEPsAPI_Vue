cebb9cd110fdb561c12b3b3b18ce602095927b02
Carlos Eduardo Correia de Souza <CarlosCorreia1002@gmail.com>
2022-10-19 14:59:51 -0300

    update 19/10
    bug in search table continues

--- indexcontroller.js
+++ indexcontroller.js
@@ -35,53 +35,52 @@ const vcep = {
                 this.estado = this.estados[19].sigla
                 this.pegaCidade(this.estado)
             }) 
-
     },
     template:
         `
-        <div class="container border rounded mt-4 mb-4 pt-2 pb-3 w-25 min-vh-25 min-vw-25">
-            <div class="d-flex flex-column pb-3 w-25">
-                <span>Estado:</span>
-                <select class="form-select form-select-sm" v-model="estado" @change="pegaCidade(estado)">
-                    <option v-for="c in this.estados">{{c.sigla}}</option>
-                </select>
-            </div>
+            <div class="container border rounded mt-4 mb-4 pt-2 pb-3 w-25 min-vh-25 min-vw-25">
+                <div class="d-flex flex-column pb-3 w-25">
+                    <span>Estado:</span>
+                    <select class="form-select form-select-sm" v-model="estado" @change="pegaCidade(estado)">
+                        <option v-for="c in this.estados">{{c.sigla}}</option>
+                    </select>
+                </div>
 
-            <div class="d-flex flex-column pb-3 w-100">
-                <span>Cidade:</span>
-                <select class="form-select form-select-sm" v-model="cidade">
-                    <option v-for="c in this.cidades">{{c.nome}}</option>
-                </select>       
+                <div class="d-flex flex-column pb-3 w-100">
+                    <span>Cidade:</span>
+                    <select class="form-select form-select-sm" v-model="cidade">
+                        <option v-for="c in this.cidades">{{c.nome}}</option>
+                    </select>       
+                </div>
+                
+                <div class="d-flex flex-column pb-3 w-100">
+                    <span>Digite uma referência:</span>
+                    <input name="referencia" type="text" v-model="referencia" size="30"/> 
+                </div>
+
+                <button @click="carregaCep()" class="d-grid gap-2 col-6 mx-auto btn btn-outline-dark">buscar CEPs</button>
             </div>
             
-            <div class="d-flex flex-column pb-3 w-100">
-                <span>Digite uma referência:</span>
-                <input name="referencia" type="text" v-model="referencia" size="30"/> 
+            <div>
+                <table class="table table-striped table-hover border w-75 min-vh-75 min-vw-75 position-absolute top-50 start-50 translate-middle mt-5 mb-4">
+                    <thead>
+                        <tr>
+                            <th scope="col">CEP</th>
+                            <th scope="col">Logradouro</th>
+                            <th scope="col">Complemento</th>
+                            <th scope="col">Bairro</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="c in this.info">
+                            <td>{{c.cep}}</td>
+                            <td>{{c.logradouro}} </td>
+                            <td>{{c.complemento}} </td>
+                            <td>{{c.bairro}} </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
-
-            <button @click="carregaCep()" class="d-grid gap-2 col-6 mx-auto btn btn-outline-dark">buscar CEPs</button>
-        </div>
-
-        <div>
-            <table class="table table-striped table-hover border w-75 min-vh-75 min-vw-75 position-absolute top-50 start-50 translate-middle mt-5 mb-4">
-                <thead>
-                    <tr>
-                        <th scope="col">CEP</th>
-                        <th scope="col">Logradouro</th>
-                        <th scope="col">Complemento</th>
-                        <th scope="col">Bairro</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="c in this.info">
-                        <td>{{c.cep}}</td>
-                        <td>{{c.logradouro}} </td>
-                        <td>{{c.complemento}} </td>
-                        <td>{{c.bairro}} </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
         `
 };
 
